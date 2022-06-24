f711b03b43aa84115e9725208025d2db96ad4ebc
Jason <officialjasonzeng@gmail.com>
2022-06-23 22:20:09 -0400

    fixed axios base url

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: "http://localhost:5000" });
+const api = axios.create({ baseURL: "/" });
 api.interceptors.request.use((req) => {
     if (localStorage.getItem("user")) {
         req.headers.Authorization = `Bearer ${JSON.parse(localStorage.getItem("user")).token}`;
